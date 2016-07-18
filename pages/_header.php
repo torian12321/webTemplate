106d1e08a61bc52f11defb1a4df71d5b49554f6c
Aitor Palomares <aitorpalomares@gmail.com>
2016-07-18 14:52:45 +0100

    Deny zoom on mobile

--- pages/_header.php
+++ pages/_header.php
@@ -12,7 +12,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0' >
 
 	<?php /* Chrome, Firefox OS, Opera and Vivaldi */?>
 	<meta name="theme-color" content="<?php echo $GLOBALS['themeColor']; ?>">
