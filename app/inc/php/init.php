734c952e5a0b18d3aba995f13e3562c2a053fd01
Aitor <aitorpalomares@hotmail.com>
2016-11-22 14:38:36 +0000

    delete php-cache
    Gulp is doing the phpToHtml, no need for this class any more

--- app/inc/php/init.php
+++ app/inc/php/init.php
@@ -13,6 +13,5 @@ header('content-type: text/html; charset=utf-8');
 require_once __dir__ . '/config.php';
 require_once __dir__ . '/func.php';
 require_once __dir__ . '/components.php';
-require_once __dir__ . '/cache.php';
 
 ?>
