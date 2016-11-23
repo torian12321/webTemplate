2a674b7cd6818e6008096115c7d0f8edaabe2998
Aitor Palomares <aitorpalomares@gmail.com>
2016-11-23 09:58:52 +0000

    new css path name

--- app/styleguide/js/config.js
+++ app/styleguide/js/config.js
@@ -1,7 +1,7 @@
 define({
 	// This css path is used as a default by the Kalei project it self.
 	//css_path: window.location.protocol + '//' + window.location.hostname + (window.location.port === '' ? '' : ':'+ window.location.port) + window.location.pathname +  'css/styles.css',
-	css_path: window.location.protocol + '//' + window.location.hostname + (window.location.port === '' ? '' : ':'+ window.location.port) + window.location.pathname +  '../src/less/style.less',
+	css_path: window.location.protocol + '//' + window.location.hostname + (window.location.port === '' ? '' : ':'+ window.location.port) + window.location.pathname +  '../inc/less/style.less',
 
 	// You can configure any path by just deleting the one above and uncommenting the one below to point at your css directory
 	// css_path: 'http://localhost/kaleistyleguide/css/styles.css',
