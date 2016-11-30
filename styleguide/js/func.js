52fa3984808230e2582d04df3298571957d7c360
Aitor <aitorpalomares@hotmail.com>
2016-11-30 10:50:06 +0000

    highlignt style

--- styleguide/js/func.js
+++ styleguide/js/func.js
@@ -6,6 +6,9 @@ page       = _doc.getElementById('stylguide_page'),
 menuOpeners= _doc.querySelectorAll('.stylguide-toggle_sidePanel'),
 init = function(){
     menu_func();
+    hljs.initHighlightingOnLoad();
+    
+    //console.log(printHTML('<h1>Hello world</h1>'));
 };
 
 
@@ -22,5 +25,14 @@ menu_func = function(){
     });
 };
 
+
+
+var
+htmlEntities = function(str) {
+    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+},
+printHTML = function(str){
+    return '<pre><code class="html">' + htmlEntities(str) + '</code></pre>'
+};
 
 // less.modifyVars({ themeColor_1 : 'black' });
